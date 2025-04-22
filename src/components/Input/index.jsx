62f96c621997e32ab8553249b00e8a8185489718
Rabb <rabb.pham@nfcguest.com>
2025-04-22 22:01:04 +0700

    modify tool

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -2,19 +2,13 @@ import React from 'react';
 
 function CustomInput({ label, helperText, questionLabel, ...props }) {
   return (
-    <div className="flex flex-col">
-      <label className="font-[700] text-[19px] mb-[10px] dark:text-[#F4F4FA]">{label}</label>
-      <label className="font-[400] text-[14px] mb-[10px] text-[#2D2D30] dark:text-[#E5E5EC]">
-        {questionLabel}
-      </label>
-
-      <input
-        className="border p-2 w-full mt-1 rounded focus:ring-2 focus:ring-blue-400 dark:border-[#73737E] dark:text-[#F4F4FA]"
-        {...props}
-      ></input>
-      {helperText && (
-        <span className="text-[12px] text-[#73737E] dark:text-[#9D9DAB] mt-1">{helperText}</span>
-      )}
+    <div class="FormGroup">
+      <div class="Subhead">{label}</div>
+      <div class="Text">{questionLabel}</div>
+      <div class="FormInput">
+        <input class="Focus" name="Preferences" type="text" {...props} />
+      </div>
+      {helperText && <div class="Disclaimer">{helperText}</div>}
     </div>
   );
 }
