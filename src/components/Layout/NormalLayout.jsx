1b563738f8cf9aa1104fe179d635bebc62f61296
Rabb <rabb.pham@nfcguest.com>
2025-04-23 11:34:13 +0700

    chagne layout

--- src/components/Layout/NormalLayout.jsx
+++ src/components/Layout/NormalLayout.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import logoColor from '../../assets/Images/Logo-Color.svg'; // Adjust path as needed
-
+import logoWhite from '../../assets/Images/Logo-white.svg'; // Adjust path as needed
+import { useTheme } from '../../contexts/ThemeContext';
+import { Link } from 'react-router-dom';
 function NormalLayout({ children }) {
+  const { theme } = useTheme();
+
+  const logoSrc = theme === 'dark' ? logoWhite : logoColor;
+
   return (
     <div id="Page">
       <div class="Masthead StickyTop">
-        <a href="Home">
-          <img alt="Enoch AI" class="Logo" src={logoColor} />
-        </a>
+        <Link to="/Home">
+          <img alt="Enoch AI" class="Logo" src={logoSrc} />
+        </Link>
       </div>
 
       {children}
