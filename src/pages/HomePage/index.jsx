1ae8acb3c98d2293fd832f4591d40cf5eae21453
ese-nguyen <pmdn.nguyen@gmail.com>
2025-04-25 22:56:37 +0700

    fix source image

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,7 +3,15 @@ import Modal from '../../components/Modal';
 import { useSelector } from 'react-redux';
 import ToolItem from '../../components/ToolItem';
 import { Link } from 'react-router-dom';
-
+import ToolSummaryImg from '../../assets/Tool-Summary.jpg';
+import DailyMealPlannerImg from '../../assets/Tool-Daily-Meal-Planner.jpg';
+import SupplementsImg from '../../assets/Tool-Supplements-and-Ingredients.jpg';
+import SimplifyJournalsImg from '../../assets/Tool-Simplify-Scientific-Journals.jpg';
+import GroceryCoachImg from '../../assets/Tool-Grocery-Shopping-Coach.jpg';
+import WellnessPlanImg from '../../assets/Tool-Personalized-Wellness-Plan.jpg';
+import LongevityRoadmapImg from '../../assets/Tool-Longevity-Roadmap.jpg';
+import MasterGardenerImg from '../../assets/Tool-Master-Gardener.jpg';
+import IngredientCheckerImg from '../../assets/Tool-Ingredient-Checker.jpg';
 const mockList = [
   {
     title: 'Chat with Enoch AI',
@@ -14,63 +22,63 @@ const mockList = [
   },
   {
     title: 'Enoch Text Summarizer',
-    imageSrc: 'src\\assets\\Tool-Summary.jpg',
+    imageSrc: ToolSummaryImg,
     link: '/tools/summarizer',
     tierAllow: ['Gold', 'Platinum', 'Silver', 'Bronze'],
     toolName: 'text-summarizer',
   },
   {
     title: 'Daily Meal Planner',
-    imageSrc: 'src\\assets\\Tool-Daily-Meal-Planner.jpg',
+    imageSrc: DailyMealPlannerImg,
     link: '/tools/planner',
     tierAllow: ['Gold', 'Platinum', 'Silver'],
     toolName: 'daily-meal-planner',
   },
   {
     title: 'Natural Supplements & Ingredients Finder',
-    imageSrc: 'src\\assets\\Tool-Supplements-and-Ingredients.jpg',
+    imageSrc: SupplementsImg,
     link: '/tools/finder',
     tierAllow: ['Gold', 'Platinum', 'Silver'],
     toolName: 'natural-supplements-ingredients-finder',
   },
   {
     title: 'Simplify Scientific Journals',
-    imageSrc: 'src\\assets\\Tool-Simplify-Scientific-Journals.jpg',
+    imageSrc: SimplifyJournalsImg,
     link: '/tools/journals',
     tierAllow: ['Gold', 'Platinum', 'Silver'],
     toolName: 'journals',
   },
   {
     title: 'Grocery Shopping Coach',
-    imageSrc: 'src\\assets\\Tool-Grocery-Shopping-Coach.jpg',
+    imageSrc: GroceryCoachImg,
     link: '/tools/grocery',
     tierAllow: ['Gold', 'Platinum'],
     toolName: 'grocery-shopping-coach',
   },
   {
     title: 'Personalized Wellness Plan',
-    imageSrc: 'src\\assets\\Tool-Personalized-Wellness-Plan.jpg',
+    imageSrc: WellnessPlanImg,
     link: '/tools/wellness',
     tierAllow: ['Gold', 'Platinum'],
     toolName: 'personalized-wellness-plan',
   },
   {
     title: 'Longevity Roadmap',
-    imageSrc: 'src\\assets\\Tool-Longevity-Roadmap.jpg',
+    imageSrc: LongevityRoadmapImg,
     link: '/tools/longevity',
     tierAllow: ['Gold', 'Platinum'],
     toolName: 'longevity-roadmap',
   },
   {
     title: 'Master Gardener',
-    imageSrc: 'src\\assets\\Tool-Master-Gardener.jpg',
+    imageSrc: MasterGardenerImg,
     link: '/tools/gardener',
     tierAllow: ['Gold', 'Platinum'],
     toolName: 'master-gardener',
   },
   {
     title: 'Ingredient Checker',
-    imageSrc: 'src\\assets\\Tool-Ingredient-Checker.jpg',
+    imageSrc: IngredientCheckerImg,
     link: '/tools/Ingredient',
     tierAllow: ['Gold', 'Platinum'],
     toolName: 'ingredients-checker',
