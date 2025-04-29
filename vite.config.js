12362d26b3dc790a19c5043f5673b5eb3be8bebd
ese-nguyen <pmdn.nguyen@gmail.com>
2025-04-29 09:27:59 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [tailwindcss()],
   server: {
-    allowedHosts: ['enochvip.consumerwellness.org', 'localhost'],
+    allowedHosts: ['enochvip.consumerwellness.org', 'localhost', "api-enochvip.consumerwellness.org"],
   },
 });
